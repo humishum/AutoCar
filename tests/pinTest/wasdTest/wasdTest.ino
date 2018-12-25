91ca5ab8407d355d4cbb663572765ab27a90cd71
Humdaan Mustafa <humishum@gmail.com>
2018-12-24 18:04:26 -0800

    first commit

--- tests/pinTest/wasdTest/wasdTest.ino
+++ tests/pinTest/wasdTest/wasdTest.ino
@@ -1,14 +1,43 @@
+//import processing.serial.*; //imports processing library 
+
 char incomingByte;   // for incoming serial data
 
+int rightPin=10;
+int leftPin=11;
+int upPin=12;
+int downPin=13;
+
 void setup() {
-        Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
-        pinMode(13,OUTPUT);
-        digitalWrite(13, LOW);
+        //pinModes for 4 directions
+        pinMode(rightPin,OUTPUT);
+        pinMode(leftPin,OUTPUT);
+        pinMode(upPin,OUTPUT);
+        pinMode(downPin,OUTPUT);
+        //begin serial port at 9600
+        Serial.begin(9600);
 }
 
 void loop() {
+  //Serial port;
 
-        // send data only when you receive data:
+  if(keyPressed){
+    if(key=='w'|| key=='W'){//Up
+      digitalWrite(13, HIGH);
+    }else if(key=='s'|| key=='S'){//Down
+      digitalWrite(12, HIGH);
+    }else if(key=='d'|| key=='D'){//Right
+      digitalWrite(11, HIGH);
+    }else if(key=='a'|| key=='A'){//Left
+      digitalWrite(10, HIGH);
+    }
+  }else{
+    digitalWrite(13, LOW);
+    digitalWrite(12, LOW);
+    digitalWrite(11, LOW);
+    digitalWrite(10, LOW);
+  }
+
+     /*   // send data only when you receive data:
         if (Serial.available() ) {
                 // read the incoming byte:
                 incomingByte = Serial.read();
@@ -25,6 +54,6 @@ void loop() {
         else
         {
         digitalWrite(13, LOW);
-        } 
+        } */
           
 }
